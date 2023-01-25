c927ff058eb57b57b48ab9b0c52b3226d245151f
User <user@Users-MacBook-Pro.local>
2023-01-25 13:48:43 +0300

    Работы со списками. Преобразование массива объектов в массив React элементов

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./components/styles/App.css";
 import PostItem from './components/PostItem';
+import PostList from './components/PostList'
 
 
 function App() {
 
 
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
+  const [posts1, setPosts1] = useState ([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
+
+  const [posts2, setPosts2] = useState ([
+    {id: 1, title: 'Python 1', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ])
+
+  const [posts3, setPosts3] = useState ([
+    {id: 1, title: 'C# 1', body: 'Description'},
+    {id: 2, title: 'C# 2', body: 'Description'},
+    {id: 3, title: 'C# 3', body: 'Description'},
+  ])
 
 
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
-      <PostItem post={{id: 2, title: 'C#', body: 'Description'}} />
-      <PostItem post={{id: 3, title: 'TypeScript', body: 'Description'}} />
-      <PostItem post={{id: 4, title: 'Java', body: 'Description'}} />
+      <PostList posts={posts1} title="Посты про JS"/>
+      <PostList posts={posts2} title="Посты про Python"/>
+      <PostList posts={posts3} title="Посты про C#"/>
+      
     </div>
   );
 }
