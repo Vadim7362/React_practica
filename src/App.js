756548201caec1938333f64dcb87476f2c90a9fa
User <user@Users-MacBook-Pro.local>
2023-01-26 09:52:55 +0300

    Управляемые и неуправляемые компоненты

--- src/App.js
+++ src/App.js
@@ -15,25 +15,14 @@ function App() {
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
 
-  const [posts2, setPosts2] = useState ([
-    {id: 1, title: 'Python 1', body: 'Description'},
-    {id: 2, title: 'Python 2', body: 'Description'},
-    {id: 3, title: 'Python 3', body: 'Description'},
-  ])
-
-  const [posts3, setPosts3] = useState ([
-    {id: 1, title: 'C# 1', body: 'Description'},
-    {id: 2, title: 'C# 2', body: 'Description'},
-    {id: 3, title: 'C# 3', body: 'Description'},
-  ])
-
-
-
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <button>Создать пост</button>
+      </form>
       <PostList posts={posts1} title="Посты про JS"/>
-      <PostList posts={posts2} title="Посты про Python"/>
-      <PostList posts={posts3} title="Посты про C#"/>
       
     </div>
   );
