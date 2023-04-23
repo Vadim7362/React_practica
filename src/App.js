cc5b6869a391e7d4ef65ccb9d6ea10fdac9c0791
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-04-23 11:10:10 +0300

    create new posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, {useState, useRef} from 'react';
-// import Counter from "./components/Counter";
-// import ClassCounter from "./components/ClassCounter";
+import React, {useState} from 'react';
 import "./styles/App.css";
-// import PostItem from './components/PostItem';
 import PostList from './components/PostList'
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
@@ -17,33 +14,29 @@ function App() {
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
 
-  const [title, setTitle] = useState('');
+  const [post, setPost] = useState({title: '', body: ''});
 
-  const bodyInputRef = useRef();
 
   const addNewPost = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(bodyInputRef.current.value)
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
   }
 
   return (
     <div className="App">
       <form>
 
-        {/* Управляемый компонент */}
-
         <MyInput 
-          value={title} 
-          onChange={e => setTitle(e.target.value)}
+          value={post.title} 
+          onChange={e => setPost({...post, title: e.target.value})}
           type="text" 
           placeholder="Название поста"
         />
 
-        {/* Неуправляемый/Не контролируемый компонент */}
-
         <MyInput 
-          ref={bodyInputRef}
+          value={post.body} 
+          onChange={e => setPost({...post, body: e.target.value})}
           type="text" 
           placeholder="Описание поста"
         />
