cd113f7024dd4c90e42f874bb6f63e0eb4c411e7
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-05-23 23:48:37 +0300

    React router. Постраничная навигация. BrowserRouter. Routes. Route. Navigate

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <h1>
+      Это приложение создано для прокачки REACT skills!!!
+    </h1>
+  );
+};
+
+export default About;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <h1 style={{color: 'red'}}>
+      ВЫ ПЕРЕШЛИ НА НЕСУЩЕСТВУЮЩУЮ СТРАНИЦУ!!!!
+    </h1>
+  );
+};
+
+export default Error;
