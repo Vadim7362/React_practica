67649b2eae131df42f64afbdd498f9e6b9be2d7d
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-05-27 20:27:57 +0300

    Улучшение навигации

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,20 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import "../styles/App.css";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import {routes} from "../router"
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/about' element={<About />} />
-      <Route path='/posts' element={<Posts />} />
-      <Route path='/posts/:id' element={<PostIdPage />} />
-      <Route path="/error" element={<Error />} />
-      <Route path="/*" element={<Navigate to="/error" replace />} />
-    </Routes>
-  )
+            {routes.map((route, index) => {
+                
+                    <Route key = {index} path = {route.path}>
+                        element={<route.element />}
+                    </Route>
+            })}
+            <Route path = "/*" element = {<Navigate to = "/posts" replace/>}/>
+        </Routes>
+        )
 }
 
 export default AppRouter
